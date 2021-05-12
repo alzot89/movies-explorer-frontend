626946841da2241db9fee30418bf5ca6f1c05ac8
Sanches <sanches-zotov@yandex.ru>
2021-05-12 17:00:44 +0300

    feat: добавить компонент ProtectedRoute

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,28 @@
 import './app.css';
+import Main from '../Main/Main';
+import { Route, Switch } from 'react-router-dom';
+import ProtectedRoute from "./ProtectedRoute";
 
 function App() {
   return (
     <div className="app">
+      <Header />
+      <Switch>
+        <Route path="/sign-up">
+          {/* <Register /> */}
+        </Route >
+        <Route path="/sign-in">
+          {/* <Login /> */}
+        </Route >
+        <ProtectedRoute
+          path="/"
+          // loggedIn={loggedIn}
+          component={Main}
+        // onEditProfile={handleEditProfileClick} onAddPlace={handleAddPlaceClick} onEditAvatar={handleEditAvatarClick}
+        // onCardClick={handleCardClick} isLoading={isLoading} cards={cards} onCardLike={handleCardLike} onCardDelete={handleConfirmDeletion}
+        />
+      </Switch>
+      <Footer />
     </div>
   );
 }
