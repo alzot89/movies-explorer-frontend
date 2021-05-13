378840ef8c28b50f32febd40c38d95d09cedcd65
Sanches <sanches-zotov@yandex.ru>
2021-05-13 13:12:51 +0300

    feat: добавить компонент Promo

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,8 +1,10 @@
-import './main.css'
+import './content.css';
+import Promo from '../Promo/Promo';
 
 function Main() {
     return (
         <main className="content">
+            <Promo />
         </main>
     );
 }
