15529f0f289c8371c127a8ea90d1e59caea556a5
Sanches <sanches-zotov@yandex.ru>
2021-05-18 18:32:16 +0300

    feat: адаптировать компонент Main под разыне экраны

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -5,15 +5,17 @@ function AboutProject() {
         <section className="about-project" id="about">
             <h2 className="about-project__title">О проекте</h2>
             <div className="about-project__line"></div>
-            <h3 className="about-project__subtitle">Дипломный проект включал 5 этапов</h3>
-            <h3 className="about-project__subtitle">На выполнение диплома ушло 5 недель</h3>
-            <p className="about-project__paragraph">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
-            <p className="about-project__paragraph">У каждого этапа был мягкий и жесткий дедлайн, которые нужно было соблюдать чтобы успешно защититься.</p>
+            <div className="about-project__container">
+                <h3 className="about-project__subtitle">Дипломный проект включал 5 этапов</h3>
+                <h3 className="about-project__subtitle about-project__subtitle_weeks">На выполнение диплома ушло 5 недель</h3>
+                <p className="about-project__paragraph">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+                <p className="about-project__paragraph">У каждого этапа был мягкий и жесткий дедлайн, которые нужно было соблюдать чтобы успешно защититься.</p>
+            </div>
             <div className="about-project__progress-bar">
                 <p className="about-project__one-week">1 неделя</p>
                 <p className="about-project__four-weeks">4 недели</p>
             </div>
-            <div className="about-project__container">
+            <div className="about-project__milestones">
                 <p className="about-project__backend">Back-end</p>
                 <p className="about-project__frontend">Front-end</p>
             </div>
--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -3,7 +3,8 @@ import './promo.css';
 function Promo() {
     return (
         <section className="promo">
-            <h1 className="promo__title">Учебный проект факультета Веб-разработки.</h1>
+            <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
+            <div className="promo__logo"></div>
         </section>
     );
 }
