01e568a3717f52b695837f2ff71008375fe53cbd
Sanches <sanches-zotov@yandex.ru>
2021-05-28 00:42:37 +0300

    fix: установить иконку удаления карточки с фильмом

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Route } from 'react-router';
 import './movies-card.css';
 
 
@@ -14,7 +15,12 @@ function MoviesCard({ movie }) {
             <img src={`https://api.nomoreparties.co${movie.image.url}`} alt={movie.nameRU} className="movie__img"></img>
             <div className="movie__info">
                 <h4 className="movie__title">{movie.nameRU}</h4>
-                <button className={`movie__button ${saved && 'movie__button_active'}`} onClick={handleMovieButton}></button>
+                <Route path="/movies">
+                    <button className={`movie__button ${saved && 'movie__button_active'}`} onClick={handleMovieButton}></button>
+                </Route>
+                <Route path="/saved-movies">
+                    <button className="movie__button movie__button_delete"></button>
+                </Route>
                 <p className="movie__duration">{`${Math.floor(movie.duration / 60)}ч${movie.duration % 60}м`}</p>
             </div>
         </li>
