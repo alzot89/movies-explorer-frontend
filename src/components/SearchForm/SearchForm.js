4ba3023ae98eb627701a396db4da16a30255ec47
Sanches <sanches-zotov@yandex.ru>
2021-05-27 15:43:07 +0300

    fix: input сделать required, установить focus

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -6,7 +6,7 @@ function SearchForm() {
         <section className="search">
             <form className="search__form">
                 <div className="search__container">
-                    <input className="search__input" name="movies" type="text" id="search" placeholder="Фильм"></input>
+                    <input className="search__input" name="movies" type="text" id="search" placeholder="Фильм" required></input>
                     <span id="search__error" className="search__error"></span>
                 </div>
                 <button className="search__button">Найти</button>
