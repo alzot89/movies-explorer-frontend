414503f1b33075b6950834031162db3d94b0ef29
Sanches <sanches-zotov@yandex.ru>
2021-06-09 12:44:31 +0300

    feat: добавлять карточки по клику

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,15 +1,25 @@
 import './movies-card-list.css';
 import MoviesCard from '../MoviesCard/MoviesCard';
 import { Route } from 'react-router-dom';
+import { useState } from 'react';
 
 function MoviesCardList({ movies }) {
+
+    const [moviesAmount, setMoviesAmount] = useState(5)
+
+
+    function handleClick() {
+        setMoviesAmount(moviesAmount + 5)
+    }
+
+
     return (
         <>
             <ul className="movies__list">
-                {movies.slice(0, 10).map((movie) => { return (<MoviesCard movie={movie} key={movie.id} />) })}
+                {movies.slice(0, moviesAmount).map((movie) => { return (<MoviesCard movie={movie} key={movie.id} />) })}
             </ul>
             <Route path="/movies">
-                <button className="movies__button">Еще</button>
+                <button className={`movies__button ${moviesAmount >= movies.length && 'movies__button_hidden'}`} onClick={handleClick}>Еще</button>
             </Route>
         </>
     );
