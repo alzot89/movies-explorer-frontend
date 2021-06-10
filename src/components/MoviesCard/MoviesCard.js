1909a83c98f8da5ce6cbb9b6e8de56d489e60c33
Sanches <sanches-zotov@yandex.ru>
2021-06-10 13:05:25 +0300

    feat: отправлять запросы на сохранение/удаление фильмов

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { Route } from 'react-router';
 import './movies-card.css';
+import mainApi from '../../utils/MainApi'
+
 
 
 function MoviesCard({ movie }) {
@@ -8,9 +10,27 @@ function MoviesCard({ movie }) {
     const BASE_URL = 'https://api.nomoreparties.co';
     const imageURL = movie.image ? BASE_URL + movie.image.url : 'нет картинки';
     const duration = `${Math.floor(movie.duration / 60)}ч${movie.duration % 60}м`;
+    const movieData = {
+        movieId: movie.id,
+        nameRU: movie.nameRU,
+        nameEN: movie.nameEN,
+        director: movie.director,
+        country: movie.country,
+        year: movie.year,
+        duration: movie.duration,
+        description: movie.description,
+        trailer: movie.trailerLink,
+        image: imageURL,
+        thumbnail: movie.image.thumbnail
+    }
 
-    function handleMovieButton() {
+    function saveMovie() {
         setSaved(!saved)
+        if (!saved) {
+            mainApi.saveMovie(movieData)
+        } else {
+            mainApi.deleteMovie(movie.id)
+        }
     }
 
     return (
@@ -19,7 +39,7 @@ function MoviesCard({ movie }) {
             <div className="movie__info">
                 <h4 className="movie__title">{movie.nameRU}</h4>
                 <Route path="/movies">
-                    <button className={`movie__button ${saved && 'movie__button_active'}`} onClick={handleMovieButton}></button>
+                    <button className={`movie__button ${saved && 'movie__button_active'}`} onClick={saveMovie}></button>
                 </Route>
                 <Route path="/saved-movies">
                     <button className="movie__button movie__button_delete"></button>
